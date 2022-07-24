b64f08db5d8550b286045a19a3765708f90ad031
Marina Olivieri <mari_debenedetti@hotmail.com>
2022-07-24 20:59:09 -0300

    segunda entrega

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header id="header">
+            <h1 className="titulo">Compra tu videojuego</h1>
+            <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Contacto</a>
+            <a href="#">Ubicaccion</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
