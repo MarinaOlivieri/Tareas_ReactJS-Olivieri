d3667ac8e6b9830bb595cb298a74f1a1efada39a
Marina Olivieri <mari_debenedetti@hotmail.com>
2022-08-03 18:14:48 -0300

    Realize las mejoras.. Creo,je

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -4,6 +4,17 @@ const Contador = () => {
 
     const [contador, setContador] = useState(0)
 
+    const aumentar = () =>{
+        setContador(contador +1)
+    }
+
+    const restar = () =>{
+        setContador(contador - 1)
+    }
+
+    const limpiar = () =>{
+        setContador(0)
+    }
     return ( 
         <div className='container my-5'>
             <div className="card text-center my-5">
@@ -11,13 +22,13 @@ const Contador = () => {
                 <h1>Nuestro Contador</h1>
                     <div className="my-5">
                         <h2 className='my-5'>{contador}</h2>
-                        <button className='btn btn-success mx-3' onClick={() => setContador(contador + 1)}>
+                        <button className='btn btn-success mx-3' onClick={aumentar}>
                             Aumentar
                         </button>
-                        <button className='btn btn-danger mx-3' onClick={() => setContador(contador - 1)} disabled={contador === 0}>
+                        <button className='btn btn-danger mx-3' onClick={restar} disabled={contador === 0}>
                             Restar
                         </button>
-                        <button className='btn btn-secondary mx-3' onClick={() => setContador(0)} disabled={contador === 0}>
+                        <button className='btn btn-secondary mx-3' onClick={limpiar} disabled={contador === 0}>
                             Limpiar
                         </button>
                     </div>
